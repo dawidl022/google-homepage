6ca2ea38df7234a6fe35cd987f40ab4f7a983c9b
dawidl022 <domdaw7@gmail.com>
2021-07-06 16:07:55 +0100

    Add dummy content, styles and keyword script
    add search results, add google favicon

---
+++ scripts/results-main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const searchQuery = "build this webpage";
+const queryWords = searchQuery.split(" ");
+console.log(queryWords);
+
+
+function inQuery(word, qArr) {
+  // looks for substring in each queryWords
+
+  for (let i = 0; i < qArr.length; i++) {
+    let qWord = qArr[i];
+    if (word.includes(qWord)) return true;
+  }
+
+  return false;
+}
+
+const resultDescriptions = document.querySelectorAll(".result p");
+
+resultDescriptions.forEach(function(descr) {
+
+  let description = descr.innerText;
+  const descriptionWords = description.split(" ");
+
+  for (let i = 0; i < descriptionWords.length; i++) {
+      let word = descriptionWords[i];
+
+      if (inQuery(word, queryWords)) {
+        word = `<span class="desktop-query-word">${word}</span>`;
+        descriptionWords[i] = word;
+      }
+    }
+  descr.innerHTML = descriptionWords.join(" ");
+}) 
